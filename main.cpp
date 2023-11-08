8b9b81d97211d630047c7c54a3fbba29a10d785f
adityastr <madityaregar@gmail.com>
2023-11-08 19:33:24 +0700

    menambahkan program volume dan luas permukaan tabung

--- main.cpp
+++ main.cpp
@@ -14,7 +14,8 @@ int main()
    cout<<"1. Memfaktorkan persamaan kuadrat"<<endl;
    cout<<"2. Menentukan turunan"<<endl;
    cout<<"3. perkalian variabel sederhana (maksimum 2 variabel)"<<endl;
-   cout<<"masukkan pilihan anda:"<<endl;
+   cout<<"4. Volume dan Luas permukaan tabung"<<endl;
+   cout<<"masukkan pilihan anda: ";
    cin>>pilih;
 
    
@@ -28,11 +29,11 @@ int main()
    cout<<"============ Bentuk umum persamaan kuadarat ax^2+bx+c =============="<<endl;
    cout<<"===================================================================="<<endl;
    float a,b,c,diskriminan,x1,x2;
-   cout<<"Masukkan nilai koefisien a:"<<endl;
+   cout<<"Masukkan nilai koefisien a: ";
    cin>>a;
-   cout<<"Masukkan nilai koefisien b:"<<endl;
+   cout<<"Masukkan nilai koefisien b: ";
    cin>>b;
-   cout<<"Masukkan nilai koefisien C:"<<endl;
+   cout<<"Masukkan nilai koefisien C: ";
    cin>>c;
    diskriminan=(b*b-4*a*c);
    if (diskriminan>=0){
@@ -71,23 +72,60 @@ int main()
    char varPertama3,varKedua3,hasil3;
 
    cout<<"perkalian antar variabel"<<endl;
-   cout<<"masukan variabel pertama:"<<endl;
+   cout<<"masukan variabel pertama: ";
    cin>>varPertama3;
-   cout<<"masukan nilai pangkat variabel pertama:"<<endl;
+   cout<<"masukan nilai pangkat variabel pertama: ";
    cin>>nilaiPangkat3v1;
-   cout<<"masukan variabel kedua:"<<endl;
+   cout<<"masukan variabel kedua: ";
    cin>>varKedua3;
-   cout<<"masukan nilai pangkat variabel kedua:"<<endl;
+   cout<<"masukan nilai pangkat variabel kedua: ";
    cin>>nilaiPangkat3v2;
-   if (varPertama3==varKedua3){
+   if (varPertama3==varKedua3)
+   {
       cout<<varPertama3;cout<<"^";cout<<nilaiPangkat3v1+nilaiPangkat3v2;
    }
    else
    {
       cout<<varPertama3;cout<<"^";cout<<nilaiPangkat3v1;cout<<"+";cout<<varKedua3;cout<<"^";cout<<nilaiPangkat3v2;
    }
-
    break;
+
+   case 4:
+   float r4,t4,hasil4;
+   int pilihan4,y4;
+   cout<<"==================================================================="<<endl;
+   cout<<"============== MENGHITUNG VOLUME DAN LUAS PERMUKAAN ==============="<<endl;
+   cout<<"========================= SEBUAH TABUNG ==========================="<<endl;
+   cout<<"==================================================================="<<endl;
+   cout<<"1. Volume tabung"<<endl;
+   cout<<"2. Luas permukaan tabung"<<endl;
+   cout<<"Masukan pilihan anda: ";
+   cin>>pilihan4;
+   if (pilihan4==1){
+      cout<<"==================================================================="<<endl;
+      cout<<"===================== MENGHITUNG VOLUME TABUNG ===================="<<endl;
+      cout<<"==================================================================="<<endl;
+      cout<<"masukkan jari jari tabung: ";
+      cin>>r4;
+      cout<<"masukkan tinggi tabung: ";
+      cin>>t4;
+      hasil4=22/7*r4*r4*t4;
+      cout<<"volume tabung = ";cout<<hasil4;
+   }
+   else
+   {
+      cout<<"==================================================================="<<endl;
+      cout<<"================= MENGHITUNG LUAS PERMUKAAN TABUNG ================"<<endl;
+      cout<<"==================================================================="<<endl;
+      cout<<"masukkan jari jari tabung: ";
+      cin>>r4;
+      cout<<"masukkan tinggi tabung: ";
+      cin>>t4;
+      hasil4=2*22/7*r4*(r4+t4);
+      cout<<"luas permukaan tabung = ";cout<<hasil4;
+   }
+   break;
+
    default:cout<<"pilihan yang anda maukkan salah";
    }
    
